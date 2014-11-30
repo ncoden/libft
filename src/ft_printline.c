718a6845dcc7128a06b98a6e12593f04c178ef04
Nicolas CODEN <nicos.coden@neuf.fr>
2014-11-30 19:55:00 +0100

    ft_printline - bug correction after backup

--- src/ft_printline.c
+++ src/ft_printline.c
@@ -6,25 +6,25 @@
 /*   By: ncoden <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/25 18:48:21 by ncoden            #+#    #+#             */
-/*   Updated: 2014/11/28 12:22:08 by ncoden           ###   ########.fr       */
+/*   Updated: 2014/11/30 15:24:49 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+	#include <stdio.h>
 void		ft_printline(t_env *e, t_pos *from, t_pos *to, int color)
 {
 	int		len;
 	double	xang;
 	double	yang;
 
-	len = MAX((to->x - from->x), (to->y - from->y));
-	xang = MIN((double)(from->x - to->x) / (double)(from->y - to->y), 1);
-	yang = MIN((double)(from->y - to->y) / (double)(from->x - to->x), 1);
+	len = MAX(ABS(to->x - from->x), ABS(to->y - from->y));
+	xang = LIM((double)(from->x - to->x) / (double)ABS(from->y - to->y), -1, 1);
+	yang = LIM((double)(from->y - to->y) / (double)ABS(from->x - to->x), -1, 1);
 	while (len >= 0)
 	{
-		mlx_pixel_put(e->mlx, e->win, from->x + len * xang,
-			from->y + len * yang, color);
+		mlx_pixel_put(e->mlx, e->win, to->x + len * xang,
+			to->y + len * yang, color);
 		len--;
 	}
 }
