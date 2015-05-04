4c5fbf1afe6474b2c95dd54ef83bb7e5a3c24d7a
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-04 22:46:30 +0200

    ft_putnbr_fd - Fix bad recursive call

--- src/nbr/ft_putnbr_fd.c
+++ src/nbr/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 08:29:52 by ncoden            #+#    #+#             */
-/*   Updated: 2015/04/22 16:12:47 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/04 19:34:52 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	ft_putnbr_fd(int nbr, int fd)
 {
 	if (nbr < 0)
 	{
-		ft_putchr('-');
+		ft_putchr_fd('-', fd);
 		if (nbr <= -10)
-			ft_putnbr(-(nbr / 10));
+			ft_putnbr_fd(-(nbr / 10), fd);
 		ft_putchr_fd('0' - (nbr % 10), fd);
 	}
 	else if (nbr < 10)
 		ft_putchr_fd('0' + nbr, fd);
 	else
 	{
-		ft_putnbr(nbr / 10);
+		ft_putnbr_fd(nbr / 10, fd);
 		ft_putchr_fd('0' + (nbr % 10), fd);
 	}
 }
