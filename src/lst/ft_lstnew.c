0f26a7e29293a71ce519275ea5b507a111372bd3
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-12 23:35:05 +0200

    Norm

--- src/lst/ft_lstnew.c
+++ src/lst/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 15:38:29 by ncoden            #+#    #+#             */
-/*   Updated: 2014/12/03 17:11:12 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/12 22:51:48 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_lst	*ft_lstnew(void const *data, size_t size)
+t_lst			*ft_lstnew(void const *data, size_t size)
 {
 	t_lst	*lst;
 
