cd28115727ae449b495ffdc93ea23dae046feceb
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-13 16:37:49 +0200

    Replace ft_lstpush* by ft_lstln* behavior (2)

--- includes/libft_evnt.h
+++ includes/libft_evnt.h
@@ -6,14 +6,14 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/22 10:21:17 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/12 22:48:50 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/13 16:32:38 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_EVNT_H
 # define LIBFT_EVNT_H
 
-# include "libft_ilst.h"
+# include "libft_hash.h"
 
 typedef struct	s_evnt
 {
@@ -21,11 +21,11 @@ typedef struct	s_evnt
 	void		*data;
 }				t_evnt;
 
-typedef struct	s_ilst_evnt
+typedef struct	s_hash_evnt
 {
-	EXTEND_ILST	(s_ilst_evnt);
+	EXTEND_HASH	(s_hash_evnt);
 	t_evnt		event;
-}				t_ilst_evnt;
+}				t_hash_evnt;
 
 t_evnt			*ft_evntnew(void (*func)(void *), void *data);
 void			ft_evntset(t_evnt *event, void (*func)(void *), void *data);
