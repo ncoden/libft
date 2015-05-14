3a2367ddd27109bbfa891038bd57eae7e9264c58
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-14 02:00:23 +0200

    ft_evntnew - Protect

--- src/evnt/ft_evntnew.c
+++ src/evnt/ft_evntnew.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/08 12:26:49 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/12 22:48:39 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/14 01:58:00 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_evnt			*ft_evntnew(void (*func)(void *), void *data)
 {
 	t_evnt		*event;
 
+	if (!func)
+		return (NULL);
 	if (!(event = (t_evnt *)malloc(sizeof(t_evnt))))
 		return (NULL);
 	event->func = func;
