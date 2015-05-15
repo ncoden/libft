7353b79859e24b3f64d09f8e84f5383d04a85b9f
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-15 17:00:08 +0200

    Add kist push functions in header

--- includes/libft_klst.h
+++ includes/libft_klst.h
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/22 10:21:17 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/14 17:00:16 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/15 16:50:30 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ typedef struct	s_klst
 }				t_klst;
 
 t_klst			*ft_klstnew(char *key, void *data, size_t size);
+t_klst			*ft_klstpushback(t_klst **alst, char *key, void *data,
+					size_t size);
+t_klst			*ft_klstpushfront(t_klst **alst, char *key, void *data,
+					size_t size);
+
 t_klst			*ft_klstget(t_klst *lst, char *key);
 
 # define		ft_klstget__(type, l, k) (type *)ft_klstget((t_klst *)l, k)
