061b20c344501627fa3fdebabc6a40789cac6441
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-19 18:04:52 +0200

    ft_sgnlmerge - don't modify source list

--- src/sgnl/ft_sgnlmerge.c
+++ src/sgnl/ft_sgnlmerge.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/17 18:27:41 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/19 18:02:24 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/19 18:04:10 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void			ft_sgnlmerge(t_ilst_evnt *events)
 {
-	ft_ilstmerge((t_ilst **)&g_sgnl, (t_ilst *)events);
+	ft_ilstinsert((t_ilst **)&g_sgnl, (t_ilst *)events);
 	while (events != NULL)
 	{
 		signal(events->index, (void (*)(int))&ft_sgnltrigger);
