a2301921e37925bcc77fd2326249d13ff6a2792d
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-24 00:37:56 +0200

    Change the ilst content (2)

--- src/trm/ft_trmsignalhook.c
+++ src/trm/ft_trmsignalhook.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/14 17:12:55 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/18 16:04:52 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/23 23:50:08 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void			ft_trmsignalhook(t_trm *trm, char sig, void (*func)(void *),
 {
 	t_ilst_evnt	*event;
 
-	if ((event = (t_ilst_evnt *)ft_ilstpushfront__(sizeof(t_ilst_evnt),
+	if ((event = (t_ilst_evnt *)ft_ilstpush__(sizeof(t_ilst_evnt),
 			(t_ilst **)&trm->on_signal, sig)))
 	{
 		event->event.func = func;
