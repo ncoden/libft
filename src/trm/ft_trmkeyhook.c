5ce1f733cb75c4ca0dc41cc1538723477ad1848d
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-24 16:48:24 +0200

    ft_trmkeyhook - Use new klst behavior

--- src/trm/ft_trmkeyhook.c
+++ src/trm/ft_trmkeyhook.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/13 19:30:12 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/13 21:45:00 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/24 16:47:19 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,12 @@
 void			ft_trmkeyhook(t_trm *trm, char *key, void (*func)(void *),
 					void *data)
 {
-	t_klst_evnt	*event;
+	t_klst_evnt		*event;
 
-	if (trm)
+	if ((event = (t_klst_evnt *)ft_klstpush__(sizeof(t_klst_evnt),
+			(t_klst **)&trm->on_key_press, key)))
 	{
-		if ((event = (t_klst_evnt *)malloc(sizeof(t_klst_evnt))))
-		{
-			event->event.func = func;
-			event->event.data = data;
-			event->key = key;
-			event->next = NULL;
-			ft_lstaddback((t_lst **)&(trm->on_key_press), (t_lst *)event);
-		}
+		event->event.func = func;
+		event->event.data = data;
 	}
 }
