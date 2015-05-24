9d0194e89cb40360af9e9d8581c57bc5bc6bff73
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-24 14:52:20 +0200

    Update ft_ilstdelone to new ilst sorted content

--- src/ilst/ft_ilstdelone.c
+++ src/ilst/ft_ilstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/13 21:06:34 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/17 19:05:04 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/24 14:50:19 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,17 @@ t_bool			ft_ilstdelone(t_ilst **alst, int index,
 	t_ilst		*lst;
 	t_ilst		*prev;
 
-	if (!alst || !func)
+	prev = ft_ilstprev(*alst, index);
+	if (prev)
+		lst = prev->next;
+	else
+		lst = *alst;
+	if (!lst || lst->index != index)
 		return (FALSE);
-	lst = *alst;
-	prev = NULL;
-	while (lst != NULL)
-	{
-		if (lst->index == index)
-		{
-			if (prev == NULL)
-				*alst = NULL;
-			else
-				prev->next = lst->next;
-			(*func)(lst);
-			return (TRUE);
-		}
-		prev = lst;
-		lst = lst->next;
-	}
-	return (FALSE);
+	if (prev)
+		prev->next = lst->next;
+	else
+		*alst = (*alst)->next;
+	(*func)(lst);
+	return (TRUE);
 }
