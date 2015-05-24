683a96454691e68a85a9815ade394695d275c423
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-24 17:51:02 +0200

    Fix ft_sgnlclrone

--- src/sgnl/ft_sgnlclrone.c
+++ src/sgnl/ft_sgnlclrone.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/16 16:06:47 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/24 17:04:19 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/24 17:50:45 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_ilst_evnt		*ft_sgnlclrone(char sig)
 	if (g_sgnl)
 	{
 		signal(sig, SIG_DFL);
-		return (ft_ilstrem((t_ilst **)&g_sgnl, sig));
+		return ((t_ilst_evnt *)ft_ilstrem((t_ilst **)&g_sgnl, sig));
 	}
 	return (NULL);
 }
