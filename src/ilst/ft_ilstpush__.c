36ca513d99ca259a1c154d1696bae3720885edf2
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-26 15:26:32 +0200

    Simplify ft_ilstpush, ft_klstpush

--- src/ilst/ft_ilstpush__.c
+++ src/ilst/ft_ilstpush__.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/23 22:45:15 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/24 15:16:12 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/26 14:18:20 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,18 @@ t_ilst			*ft_ilstpush__(size_t size, t_ilst **alst, int index)
 	t_ilst		*prev;
 
 	if ((new = ft_ilstprev(*alst, index, &prev)))
-		new->index = index;
+		return (new);
+	if (!(new = ft_ilstnew__(size, index)))
+		return (NULL);
+	if (prev)
+	{
+		new->next = prev->next;
+		prev->next = new;
+	}
 	else
 	{
-		if (!(new = ft_ilstnew__(size, index)))
-			return (NULL);
-		if (prev)
-		{
-			new->next = prev->next;
-			prev->next = new;
-		}
-		else
-		{
-			new->next = *alst;
-			(*alst) = new;
-		}
+		new->next = *alst;
+		(*alst) = new;
 	}
 	return (new);
 }
--- src/klst/ft_klstpush__.c
+++ src/klst/ft_klstpush__.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/23 22:45:15 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/24 15:16:12 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/26 14:13:35 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,18 @@ t_klst			*ft_klstpush__(size_t size, t_klst **alst, char *key)
 	t_klst		*prev;
 
 	if ((new = ft_klstprev(*alst, key, &prev)))
-		new->key = key;
+		return (new);
+	if (!(new = ft_klstnew__(size, key)))
+		return (NULL);
+	if (prev)
+	{
+		new->next = prev->next;
+		prev->next = new;
+	}
 	else
 	{
-		if (!(new = ft_klstnew__(size, key)))
-			return (NULL);
-		if (prev)
-		{
-			new->next = prev->next;
-			prev->next = new;
-		}
-		else
-		{
-			new->next = *alst;
-			(*alst) = new;
-		}
+		new->next = *alst;
+		*alst = new;
 	}
 	return (new);
 }
