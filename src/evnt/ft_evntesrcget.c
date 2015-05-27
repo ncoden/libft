1fc92f25a8e706d162b7fc78dcc00882b89d1de8
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-27 12:49:30 +0200

    Fix ft_evntsrcget
    Wrong return when type is 0 (TYPE_UNKNOW)

--- src/evnt/ft_evntesrcget.c
+++ src/evnt/ft_evntesrcget.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/27 11:40:42 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/27 12:16:55 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/27 12:35:33 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@ void			*ft_evntesrcget(t_type type)
 	t_tdata		*esrc;
 
 	if (!type)
-		return (ft_stckhead(g_evnt_esrcs));
-	esrc = (t_tdata *)ft_stcksrc(g_evnt_esrcs,
-		(t_bool (*)(void *, void *))&find_esrc, &type);
+		esrc = ft_stckhead(g_evnt_esrcs);
+	else
+		esrc = (t_tdata *)ft_stcksrc(g_evnt_esrcs,
+			(t_bool (*)(void *, void *))&find_esrc, &type);
 	if (esrc)
 		return (esrc->data);
 	return (NULL);
