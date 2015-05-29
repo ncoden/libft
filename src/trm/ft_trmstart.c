4677c00fa6529962e0a67f84f262314b2736b208
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-29 15:55:23 +0200

    Optimize ft_sgnlstart
    Use sgnl stack features instead of combine lists

--- src/trm/ft_trmstart.c
+++ src/trm/ft_trmstart.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/17 22:00:29 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/28 20:35:57 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/29 15:48:42 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,26 +33,6 @@ static t_mt_tps		*start_tps(t_trm *trm)
 	return (tps);
 }
 
-static t_ilst_evnt	*build_sgnls(t_mt_tps *tps, t_ilst_evnt **esrcs)
-{
-	t_ilst_evnt		*lsgnls;
-	t_ilst_evnt		*lesrcs;
-
-	lsgnls = (t_ilst_evnt *)ft_lstdup__(sizeof(t_ilst_evnt),
-		(t_lst *)tps->trm->on_signal);
-	lesrcs = NULL;
-	tps = tps->father;
-	while (tps != NULL)
-	{
-		if (!(ft_ilstfill__(sizeof(t_ilst_evnt), (t_ilst **)&lsgnls,
-			(t_ilst *)tps->trm->on_signal)))
-			return (NULL);
-		tps = tps->father;
-	}
-	*esrcs = lesrcs;
-	return (lsgnls);
-}
-
 static void			trigger_cmd(t_mt_tps *tps, char *cmd)
 {
 	t_klst_evnt		*evnt;
@@ -74,25 +54,20 @@ void				ft_trmstart(t_trm *trm)
 {
 	t_mt_tps		*tps;
 	char			*cmd;
-	t_ilst_evnt		*sgnls;
-	t_ilst_evnt		*esrcs;
-	t_ilst_evnt		*save_sgnl;
 
 	if (trm)
 	{
-		save_sgnl = ft_sgnlget();
 		tps = start_tps(trm);
 		if (trm->on_start != NULL)
 			(*trm->on_start)(tps);
-		sgnls = build_sgnls(tps, &esrcs);
 		tps->status = TRM_STACTIVE;
 		ft_trmset(trm);
-		ft_sgnlset(sgnls);
+		ft_sgnlspush(trm->on_signal);
 		while (tps->status == TRM_STACTIVE)
 		{
 			if ((cmd = ft_read_trm()))
 				trigger_cmd(tps, cmd);
 		}
-		ft_sgnlset(save_sgnl);
+		ft_sgnlspull();
 	}
 }
