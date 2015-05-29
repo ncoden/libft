e0c194de38ef40b508a3e5ade75f9dd3450706b0
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-29 16:57:04 +0200

    Optimize ft_sgnlspush
    Don’t push NULL events in empty stack : empty stack will return NULL
    anyway

--- src/sgnl/ft_sgnlspush.c
+++ src/sgnl/ft_sgnlspush.c
@@ -6,24 +6,26 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/29 14:08:18 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/29 15:41:20 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/29 16:38:51 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+	#include <stdio.h>
 t_bool			ft_sgnlspush(t_ilst_evnt *events)
 {
-	if (ft_stckpush(&g_sgnl_stckevnts, g_sgnl_evnts))
+	if (g_sgnl_evnts || g_sgnl_stckevnts)
 	{
-		if (ft_stckpush(&g_sgnl_stckesrcs, g_sgnl_esrcs))
+		if (!(ft_stckpush(&g_sgnl_stckevnts, g_sgnl_evnts)))
+			return (FALSE);
+		if (!(ft_stckpush(&g_sgnl_stckesrcs, g_sgnl_esrcs)))
 		{
-			g_sgnl_evnts = events;
-			ft_sgnllisten(events);
-			g_sgnl_esrcs = NULL;
-			return (TRUE);
+			ft_stckpull(&g_sgnl_stckevnts);
+			return (FALSE);
 		}
-		ft_stckpull(&g_sgnl_stckevnts);
 	}
-	return (FALSE);
+	g_sgnl_evnts = events;
+	ft_sgnllisten(events);
+	g_sgnl_esrcs = NULL;
+	return (TRUE);
 }
