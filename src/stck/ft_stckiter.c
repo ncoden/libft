51a732f5554e341e19c1d2a111c63b8bab03ad63
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-29 20:45:54 +0200

    Fix ft_stckiter
    Fix bad index

--- src/stck/ft_stckiter.c
+++ src/stck/ft_stckiter.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/25 17:23:12 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/29 15:20:12 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/29 19:09:37 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void			ft_stckiter(t_stck *stck, void (*func)(void *))
 		count = stck->count;
 		while (count > 0)
 		{
-			(*func)(stck->datas[count]);
 			count--;
+			(*func)(stck->datas[count]);
 		}
 		stck = stck->next;
 	}
