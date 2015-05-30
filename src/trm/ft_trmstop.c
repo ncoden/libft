43eb3450c3cf4107f3a71592ba1b25fff1a99fc8
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-30 16:06:43 +0200

    ft_trmstop : stop childrens before

--- src/trm/ft_trmstop.c
+++ src/trm/ft_trmstop.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 01:27:42 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/30 00:18:54 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/30 16:05:24 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_bool			ft_trmstop(t_mt_tps *tps)
 {
 	if (tps || (tps = ft_esrcget(TYPE_TPS)))
 	{
+		if (tps->child)
+			ft_trmstop(tps->child);
 		if (tps->status & TRM_STREADING)
 			ft_trmclosein();
 		tps->status = 0;
