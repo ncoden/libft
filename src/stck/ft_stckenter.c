daa376a789408a429fc4d509b8e7cdbc9f799e6e
Nicolas Coden <ncoden@student.42.fr>
2015-06-06 16:09:48 +0200

    Fix ft_stckenter
    Fix ft_stckenter for the case where the stack exists but is empty

--- src/stck/ft_stckenter.c
+++ src/stck/ft_stckenter.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/04 20:53:02 by ncoden            #+#    #+#             */
-/*   Updated: 2015/06/04 21:41:39 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/06/05 18:12:03 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 t_bool			ft_stckenter(t_stck **astck)
 {
 	int			*count;
+	t_stck		*stck;
 
-	if (*astck)
+	stck = *astck;
+	if (stck && (stck->next != NULL || stck->head > 0))
 	{
 		if (!(count = (int *)malloc(sizeof(int))))
 			return (FALSE);
-		*count = (*astck)->count;
+		*count = stck->count;
 		if (!(ft_stckpush(astck, count)))
 			return (FALSE);
-		(*astck)->count = 0;
+		stck->count = 0;
 	}
 	return (TRUE);
 }
