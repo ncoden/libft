c2627961062a680e48bc5a59e2949436f07954bb
Nicolas Coden <ncoden@student.42.fr>
2015-06-06 18:37:38 +0200

    Use termcaps output in ft_trmgetout
    Use the output given by termcaps instead of “tty”.

--- src/trm/ft_trmgetout.c
+++ src/trm/ft_trmgetout.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 18:58:42 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/29 23:46:38 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/06/06 18:35:26 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int				ft_trmgetout(void)
 {
 	if (g_trm_stdout <= 0)
-		g_trm_stdout = open("/dev/tty", O_WRONLY | O_APPEND | O_CREAT);
+		g_trm_stdout = open(ttyname(0), O_WRONLY | O_APPEND | O_CREAT);
 	return (g_trm_stdout);
 }
