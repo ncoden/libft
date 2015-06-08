31cd461e4a055ec7a70287a0ea64cfaa6de6b15d
Nicolas Coden <ncoden@student.42.fr>
2015-06-08 14:04:26 +0200

    Fix t_trmstop : stop all children

--- src/trm/ft_trmstop.c
+++ src/trm/ft_trmstop.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 01:27:42 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/30 16:05:24 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/06/08 14:03:29 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 t_bool			ft_trmstop(t_mt_tps *tps)
 {
+	t_mt_tps	*child;
+
 	if (tps || (tps = ft_esrcget(TYPE_TPS)))
 	{
 		if (tps->child)
-			ft_trmstop(tps->child);
+		{
+			child = tps->child;
+			while (child)
+			{
+				ft_trmstop(child);
+				child = child->next;
+			}
+		}
 		if (tps->status & TRM_STREADING)
 			ft_trmclosein();
 		tps->status = 0;
