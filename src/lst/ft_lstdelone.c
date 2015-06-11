c36e48286c93bcec6f8f4fb177177781b72c8eef
Nicolas Coden <ncoden@student.42.fr>
2015-06-11 17:50:29 +0200

    Fix ft_lstdelone
    Only delete the given node in the list, restore the next node.

--- src/lst/ft_lstdelone.c
+++ src/lst/ft_lstdelone.c
@@ -6,19 +6,22 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 16:29:21 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/27 18:33:16 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/06/11 16:42:00 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_lst **alst, void (*del)(void *, size_t))
+void			ft_lstdelone(t_lst **alst, void (*del)(void *, size_t))
 {
-	if (*alst != NULL)
+	t_lst		*next;
+
+	if (*alst)
 	{
+		next = (*alst)->next;
 		if (del)
 			(*del)((*alst)->data, (*alst)->size);
 		free(*alst);
+		*alst = next;
 	}
-	*alst = NULL;
 }
