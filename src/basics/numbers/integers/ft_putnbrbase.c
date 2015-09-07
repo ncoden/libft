40e13a6445516a24f711d9e30bd7200de8dbb372
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-07 15:12:33 +0200

    Change Numbers/ft_putnbrbase to support -62 to 62 bases
    Add support for :
     - Negative bases -62 to -2
     - Unary base (1)
     - Bases 11 to 62
    
    The bases are made of following characters (in the order) :
     - ‘0’ to ‘9’ digits
     - ‘A’ to ‘Z’ lettres
     - ‘a’ to ‘z’ letters
    
    The bases -1 and 0 have no meaning, and don’t print anything

--- src/basics/numbers/integers/ft_putnbrbase.c
+++ src/basics/numbers/integers/ft_putnbrbase.c
@@ -6,31 +6,62 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/04 19:14:17 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/27 23:34:14 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/09/07 11:49:55 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/basics/chars.h"
+#include "libft/utils/math.h"
 
-void			ft_putnbrbase(int nbr, char base)
+static inline void	print_digit(unsigned char digit)
 {
-	if (base > 1 && base < 10)
+	ft_putchr(
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz"
+		[digit]
+	);
+}
+
+static void			print_positive_base(int nbr, int base)
+{
+	int			v;
+	int			r;
+
+	r = nbr % base;
+	v = nbr / base;
+	if (v != 0)
+		print_positive_base(v, base);
+	print_digit(ABS(r));
+}
+
+static void			print_negative_base(int nbr, int base)
+{
+	int			r;
+	int			v;
+
+	r = nbr % base;
+	v = nbr / base + (r < 0);
+	if (v != 0)
+		print_negative_base(v, base);
+	r = ((r < 0) ? r + ABS(base) : r) + '0';
+	print_digit(r);
+}
+
+void				ft_putnbrbase(int nbr, int base)
+{
+	if (base >= -62 && base <= 62)
 	{
-		if (nbr < 0)
+		if (base < -1)
+			print_negative_base(nbr, base);
+		else if (base > 0)
 		{
-			ft_putchr('-');
-			if (nbr <= -(base))
-				ft_putnbrbase(-(nbr / base), base);
-			ft_putchr('0' - (nbr % base));
-		}
-		else if (nbr < base)
-			ft_putchr('0' + nbr);
-		else
-		{
-			ft_putnbrbase(nbr / base, base);
-			ft_putchr('0' + nbr % base);
+			if (nbr < 0)
+				ft_putchr('-');
+			if (base == 1)
+				ft_putnchr('1', ABS(nbr));
+			else
+				print_positive_base(nbr, base);
 		}
 	}
-	else if (base == 1)
-		ft_putnchr('1', nbr);
 }
