249c80a87f26029ed83f978115801eb823e85a3f
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-11 19:25:55 +0200

    Strings: optimize ft_strncat
    Iterate on source and destination strings with the same index

--- include/libft/basics/strings.h
+++ include/libft/basics/strings.h
@@ -37,13 +37,13 @@ size_t			ft_strlen(const char *str);
 size_t			ft_strnlen(const char *s, size_t n);
 char			*ft_strmap(const char *s, char (*f)(char));
 char			*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-char			*ft_strncat(char *dest, const char *src, size_t nb);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strncpy(char *dest, const char *src, size_t n);
 char			*ft_strnstr(const char *str, const char *to_find, size_t size);
 char			*ft_strrchr(const char *str, int c);
 char			**ft_strsplit(const char *s, char c);
 char			*ft_strstr(const char *str, const char *to_find);
+char			*ft_strncat(char *dst, const char *src, size_t n);
 char			*ft_strsub(const char *s, size_t start, size_t n);
 char			*ft_strtrim(const char *s);
 
--- src/basics/strings/transform/ft_strncat.c
+++ src/basics/strings/transform/ft_strncat.c
@@ -6,25 +6,25 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/07/09 18:33:36 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/28 00:25:09 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/09/10 01:35:30 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft/basics/strings.h"
 
-char	*ft_strncat(char *dest, const char *src, size_t nb)
+char			*ft_strncat(char *dst, const char *src, size_t n)
 {
-	size_t	i;
-	size_t	end;
+	size_t		i;
+	char		*end;
 
 	i = 0;
-	end = ft_strlen(dest);
-	while (src[i] != '\0' && i < nb)
+	end = ft_strchr(dst, '\0');
+	while (src[i] != '\0' && i < n)
 	{
-		dest[end + i] = src[i];
+		end[i] = src[i];
 		i++;
 	}
-	dest[end + i] = '\0';
-	return (dest);
+	end[i] = '\0';
+	return (dst);
 }
