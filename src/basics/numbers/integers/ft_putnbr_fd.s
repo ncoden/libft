571cdbd5bd630977f930e25b43781e2b6787e7d0
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-11 15:06:01 +0200

    Fix Numbers/ft_putnbr
    Use the %r8 register instead of %rdx to save the file description. %rdx
    can be modified by the _ft_putchr_fd calls.

--- src/basics/numbers/integers/ft_putnbr_fd.s
+++ src/basics/numbers/integers/ft_putnbr_fd.s
@@ -6,7 +6,7 @@
 ;    By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2015/05/04 13:07:07 by ncoden            #+#    #+#              ;
-;    Updated: 2015/09/07 23:34:17 by ncoden           ###   ########.fr        ;
+;    Updated: 2015/09/09 16:55:17 by ncoden           ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -27,6 +27,8 @@ _ft_putnbr_fd:
 	push 		rax
 	push		rbx
 	push		rdx
+	push		r8
+	sub			rsp, 8
 
 	mov			ebx, edi			; Move n to an unused register
 
@@ -37,13 +39,13 @@ prepare_negative:
 	not			ebx					; Transform negative number to unsigned
 	inc			ebx					; (!negative_nbr + 1 = positive_nbr)
 
-	mov			edx, esi			; -> Save fd
-									; (_ft_putchr_fd change %rsi)
+	mov			r8d, esi			; -> Save fd
+									; (_ft_putchr_fd change %esi)
 
-	mov			rdi, '-'			; Print '-' (fd in %rsi)
+	mov			rdi, '-'			; Print '-' (fd in %esi)
 	call		_ft_putchr_fd
 
-	mov			esi, edx			; <- Retrieve fd
+	mov			esi, r8d			; <- Retrieve fd
 
 prepare_loop:
 	mov			eax, ebx			; Prepare division (number to divide in %eax)
@@ -55,7 +57,7 @@ read_loop:							; Read loop : Save number digits into stack
 	sub			edx, edx			; | - Clear edx
 	div			edi					; | - Divise %edx:%eax with %edi
 									; |
-	push		rdx					; | -> Save each modulo
+	push		rdx					; | -> Save each digit
 	sub			rsp, 8				; |    (Align stack)
 									; |
 	inc			ebx					; | - Count digit number
@@ -63,15 +65,15 @@ read_loop:							; Read loop : Save number digits into stack
 	cmp			eax, 0				; |
 	jne			read_loop			; |_
 
-	mov			edx, esi			; -> Save fd
-									; (_ft_putchr_fd change %rsi)
+	mov			r8d, esi			; -> Save fd
+									; (_ft_putchr_fd change %esi)
 
 print_loop:							; Print loop : For each digit pushed, print
 									; |				it (in reversed order)
 									; | Print digit :
 	add			rsp, 8				; |     (Align stack)
 	pop			rdi					; |  <- Retrieve each digit
-	mov			esi, edx			; |  <- Retrieve fd
+	mov			esi, r8d			; |  <- Retrieve fd
 									; |
 	add			edi, '0'			; |  - Convert to printable digit
 	call		_ft_putchr_fd		; |  - Print digit
@@ -80,6 +82,7 @@ print_loop:							; Print loop : For each digit pushed, print
 	cmp			ebx, 0				; |
 	jne			print_loop			; |_
 
+	pop			r8
 	pop			rdx					; Reset used registers
 	pop			rbx
 	pop			rax
