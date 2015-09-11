14c49cc695b6163921a6d253f913e46d4b03cb5e
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-11 19:46:08 +0200

    Strings: update ft_strncmp
    Changes:
     - Return the difference between the first different characters,
    instead of 1/0/-1.
     - Simplify the algorithm : stop at the first difference, instead of
    search it.

--- src/basics/strings/iterate/ft_strncmp.c
+++ src/basics/strings/iterate/ft_strncmp.c
@@ -6,33 +6,22 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/07/09 15:10:26 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/28 00:17:46 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/09/09 14:53:22 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int				ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t		i;
 
-	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] > s2[i])
-			return (1);
-		if (s1[i] < s2[i])
-			return (-1);
-		if ((size_t)i == (n - 1))
-			return (0);
+	i = 0;
+	while (s1[i] == s2[i]
+		&& s1[i] != '\0'
+		&& i < (n - 1))
 		i++;
-	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);
-	else if (s1[i] == '\0')
-		return (-1);
-	else
-		return (1);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
