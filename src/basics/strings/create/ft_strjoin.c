dee8be2bb11d4f9939375b63944b105d6eefc7cd
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-11 18:44:10 +0200

    Strings: update ft_strjoin
     - Manually allocate string instead of create a fresh string
     - Normalise variable names and syntax

--- src/basics/strings/create/ft_strjoin.c
+++ src/basics/strings/create/ft_strjoin.c
@@ -6,26 +6,24 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 12:22:22 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/28 00:26:06 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/09/11 17:15:18 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include "libft/basics/strings.h"
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	size_t		size1;
 	size_t		size2;
-	char		*new_str;
+	char		*ret;
 
 	size1 = ft_strlen(s1);
 	size2 = ft_strlen(s2);
-	new_str = ft_strnew(size1 + size2);
-	if (new_str != NULL)
-	{
-		ft_strcpy(new_str, s1);
-		ft_strcpy(&(new_str[size1]), s2);
-	}
-	return (new_str);
+	if (!(ret = (char *)malloc(sizeof(char) * (size1 + size2 + 1))))
+		return (NULL);
+	ft_strcpy(ret, s1);
+	ft_strcpy(&ret[size1], s2);
+	return (ret);
 }
