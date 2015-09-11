3e9ca4a1fd18c6de2ea2f94446dee7c9ca2ed6b0
Nicolas Coden <nicos.coden@neuf.fr>
2015-09-11 18:47:32 +0200

    Strings: update ft_strtrim
    Optimizations:
    - Directly create a substring instead of creating a fresh
    - Iterate on the string with start and end pointers, instead of indexes

--- src/basics/strings/create/ft_strtrim.c
+++ src/basics/strings/create/ft_strtrim.c
@@ -6,27 +6,24 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 13:09:46 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/27 23:58:51 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/09/10 11:41:02 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft/basics/strings.h"
 
-char	*ft_strtrim(char const *s)
+char			*ft_strtrim(char const *s)
 {
-	size_t		st;
-	size_t		end;
-	char		*new_str;
+	char		*end;
 
-	st = 0;
-	end = ft_strlen(s) - 1;
-	while (st < end && (s[st] == ' ' || s[st] == '\n' || s[st] == '\t'))
-		st++;
-	while (end >= st && (s[end] == ' ' || s[end] == '\n' || s[end] == '\t'))
+	end = ft_strchr(s, '\0') - 1;
+	while (s != end && (*s == ' ' || *s == '\n' || *s == '\t'))
+		s++;
+	while (s != end && (*end == ' ' || *end == '\n' || *end == '\t'))
 		end--;
-	new_str = ft_strnew(end - st + 1);
-	if (new_str != NULL)
-		ft_strncpy(new_str, &(s[st]), (end - st + 1));
-	return (new_str);
+	if (s == end)
+		return (ft_strnew(0));
+	else
+		return (ft_strsub(s, 0, end - s + 1));
 }
