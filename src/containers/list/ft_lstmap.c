7a11630d1391e837bab722e7ac6e3760d4e33153
Nicolas Coden <nicos.coden@neuf.fr>
2015-10-02 23:29:54 +0200

    List: fix ft_lstmap
    ft_lstmap allocated each list node, while it should only make a list
    with the nodes allocated by the function.

--- src/containers/list/ft_lstmap.c
+++ src/containers/list/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 18:05:20 by ncoden            #+#    #+#             */
-/*   Updated: 2015/10/02 17:40:17 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/10/02 23:29:01 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ t_lst			*ft_lstmap(t_lst *lst, t_lst *(*cmp)(t_lst *node))
 	t_lst		*new_prev;
 
 	new_begin = NULL;
+	new_prev = NULL;
 	while (lst != NULL)
 	{
-		new = (t_lst *)ft_memalloc(sizeof(t_lst));
-		if (new == NULL)
-			return (NULL);
+		new = (*cmp)(lst);
 		if (new_begin == NULL)
 			new_begin = new;
-		ft_memcpy(new, (*cmp)(lst), sizeof(t_lst));
-		new_prev->next = new;
+		else
+			new_prev->next = new;
 		new_prev = new;
 		lst = lst->next;
 	}
