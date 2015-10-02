26751df3053923b43275a746253c1243df4eba68
Nicolas Coden <nicos.coden@neuf.fr>
2015-10-02 23:21:44 +0200

    List: delete ft_lstshift, rename ft_lstdelone to ft_lstshift
    ft_lstdelone and ft_lstshift has the same behaviour.
    ft_lstdelone taked a function pointer in parameter to apply on the list
    node data.

--- src/containers/list/ft_lstshift.c
+++ src/containers/list/ft_lstshift.c
@@ -5,22 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/07 15:50:08 by ncoden            #+#    #+#             */
-/*   Updated: 2015/10/02 17:36:31 by ncoden           ###   ########.fr       */
+/*   Created: 2014/11/06 16:29:21 by ncoden            #+#    #+#             */
+/*   Updated: 2015/10/02 23:17:47 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft/containers/list.h"
 
-void			ft_lstshift(t_lst **alst)
+void			ft_lstshift(t_lst **alst, void (*del)(void *data))
 {
+	t_lst		*lst;
 	t_lst		*next;
 
-	if (*alst != NULL)
-	{
-		next = (*alst)->next;
-		free(*alst);
-		*alst = next;
-	}
+	lst = *alst;
+	if (lst != NULL)
+		return ;
+	next = lst->next;
+	if (del)
+		(*del)(lst->data);
+	free(lst);
+	*alst = next;
 }
