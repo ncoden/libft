63afba8d08ac23e502496f9d3a380ef6f0de313b
Nicolas Coden <nicos.coden@neuf.fr>
2015-10-02 23:24:37 +0200

    List: optimize ft_lstdel
    Optimise ft_lstdel by deleting the whole list without ft_lstdelone. It
    avoid to do useless operations on list pointer.

--- src/containers/list/ft_lstdel.c
+++ src/containers/list/ft_lstdel.c
@@ -6,21 +6,26 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 17:01:49 by ncoden            #+#    #+#             */
-/*   Updated: 2015/10/02 17:28:47 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/10/02 22:31:09 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include "libft/containers/list.h"
 
-void			ft_lstdel(t_lst **alst, void (*del)(void *))
+void			ft_lstdel(t_lst **alst, void (*del)(void *data))
 {
+	t_lst		*lst;
 	t_lst		*next;
 
-	while (*alst != NULL)
+	lst = *alst;
+	while (lst != NULL)
 	{
-		next = (*alst)->next;
-		ft_lstdelone(alst, del);
-		*alst = next;
+		next = lst->next;
+		if (del)
+			(*del)(lst->data);
+		free(lst);
+		lst = next;
 	}
+	*alst = NULL;
 }
