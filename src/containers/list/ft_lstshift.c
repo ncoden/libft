bce63aa494e2946dc1f493573507ddbbf53dacb2
Nicolas Coden <nicos.coden@neuf.fr>
2015-10-03 02:12:15 +0200

    List: fix ft_lstshift

--- src/containers/list/ft_lstshift.c
+++ src/containers/list/ft_lstshift.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 16:29:21 by ncoden            #+#    #+#             */
-/*   Updated: 2015/10/02 23:17:47 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/10/03 01:17:29 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void			ft_lstshift(t_lst **alst, void (*del)(void *data))
 	t_lst		*next;
 
 	lst = *alst;
-	if (lst != NULL)
+	if (lst == NULL)
 		return ;
 	next = lst->next;
 	if (del)
