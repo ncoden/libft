9282e444113738c320d54cd5c1f4718c4ddb0b95
Nicolas Coden <nicos.coden@neuf.fr>
2015-10-03 19:40:36 +0200

    List: protect ft_strsplit_tolst against Out Of Memory

--- src/containers/list/ft_strsplit_tolst.c
+++ src/containers/list/ft_strsplit_tolst.c
@@ -6,11 +6,11 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 11:15:04 by ncoden            #+#    #+#             */
-/*   Updated: 2015/10/03 18:21:45 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/10/03 18:45:26 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include "libft/basics/strings.h"
 #include "libft/containers/list.h"
 #include "libft/utils/parse.h"
@@ -18,12 +18,20 @@
 t_lst			*ft_strsplit_tolst(const char *s, char c)
 {
 	char		*word;
+	char		*cpy;
 	t_lst		*lst;
 	t_lst		*iter;
 
 	lst = NULL;
 	iter = NULL;
 	while ((word = ft_prsword(&s, c)))
-		ft_lstpushiter(&lst, &iter, ft_strsub(word, 0, s - word));
+	{
+		if (!(cpy = ft_strsub(word, 0, s - word))
+			|| !ft_lstpushiter(&lst, &iter, cpy))
+		{
+			ft_lstdel(&lst, &free);
+			return (NULL);
+		}
+	}
 	return (lst);
 }
