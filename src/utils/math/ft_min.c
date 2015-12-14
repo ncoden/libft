5286bf70eb75d94cec4bea6b983b83c1f4ad9904
Nicolas Coden <nicos.coden@neuf.fr>
2015-12-14 18:09:58 +0100

    Math: add ft_sizemin
    ft_sizemin: minimum between two size_t numbers
    
    Note (14/12/2015) : This function is added now for iostream. Math and
    Numbers will be refactored by the same way for all types and operations.

--- src/utils/math/ft_min.c
+++ src/utils/math/ft_min.c
@@ -6,11 +6,18 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/24 15:05:20 by ncoden            #+#    #+#             */
-/*   Updated: 2015/08/28 23:13:32 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/12/14 17:19:05 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_min(int nbr1, int nbr2)
+#include <stddef.h>
+
+int				ft_min(int nbr1, int nbr2)
+{
+	return ((nbr1 < nbr2) ? nbr1 : nbr2);
+}
+
+size_t			ft_sizemin(size_t nbr1, size_t nbr2)
 {
 	return ((nbr1 < nbr2) ? nbr1 : nbr2);
 }
